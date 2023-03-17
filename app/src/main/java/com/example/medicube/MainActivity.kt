69c720908d0b871b047d16384b7c68ba8ef69381
Chalani Rajapaksha <chalani.rajapaksha@elegantmedia.lk>
2023-03-18 00:48:52 +0530

    All User Management UI completed

--- app/src/main/java/com/example/medicube/MainActivity.kt
+++ app/src/main/java/com/example/medicube/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.medicube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.medicube.R
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
