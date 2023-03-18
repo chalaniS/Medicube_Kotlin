30e28f8c8c6834591b4c5268cdf870161a90ba72
Chalani Rajapaksha <chalani.rajapaksha@elegantmedia.lk>
2023-03-18 13:29:25 +0530

    All needMedicine Management UI

--- app/src/main/java/com/example/medicube/SignInActivity.kt
+++ app/src/main/java/com/example/medicube/SignInActivity.kt
@@ -24,7 +24,7 @@ class SignInActivity : AppCompatActivity() {
         binding.textView.setOnClickListener {
 //            val intent = Intent(this, SignUpActivity::class.java)
             //edit here
-            val intent = Intent(this, HomePage::class.java)
+            val intent = Intent(this, Userprofile::class.java)
 
             startActivity(intent)
         }
