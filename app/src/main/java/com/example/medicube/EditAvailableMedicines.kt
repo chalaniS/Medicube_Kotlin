6b158fa6e19300cd58837274e02bc47bd7886f3f
IT21378270 <it21378270@my.sliit.lk>
2023-03-19 17:21:34 +0530

    UI completed

---
+++ app/src/main/java/com/example/medicube/EditAvailableMedicines.kt
@@ -0,0 +1,11 @@
+package com.example.medicube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAvailableMedicines : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_available_medicines)
+    }
+}
