66107bf25192cc3bea8634a81a303e340d6a8613
IT21378270 <it21378270@my.sliit.lk>
2023-03-19 17:21:34 +0530

    UI completed

--- app/src/main/java/com/example/medicube/SignInActivity.kt
+++ app/src/main/java/com/example/medicube/SignInActivity.kt
@@ -24,7 +24,7 @@ class SignInActivity : AppCompatActivity() {
         binding.textView.setOnClickListener {
 //            val intent = Intent(this, SignUpActivity::class.java)
             //edit here
-            val intent = Intent(this, Userprofile::class.java)
+            val intent = Intent(this, AddAvailableMedicines::class.java)
 
             startActivity(intent)
         }
