c1da6afa535fed45348b8ff3cebfc4be490aab16
IT21181160 <99317617+IT21181160@users.noreply.github.com>
2023-04-29 18:41:33 +0530

    UI done

---
+++ app/src/main/java/com/example/medicube/DeleteUpdate.kt
@@ -0,0 +1,11 @@
+package com.example.medicube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeleteUpdate : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete_update)
+    }
+}
