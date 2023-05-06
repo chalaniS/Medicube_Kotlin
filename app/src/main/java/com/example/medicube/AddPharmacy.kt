fea8d2802021120413c3ed0518f69ea117d8e4be
IT21181160 <99317617+IT21181160@users.noreply.github.com>
2023-05-06 15:14:34 +0530

    Insert completed

--- app/src/main/java/com/example/medicube/AddPharmacy.kt
+++ app/src/main/java/com/example/medicube/AddPharmacy.kt
@@ -7,5 +7,14 @@ class AddPharmacy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_pharmacy)
+
+        //Not Sure
+        lateinit var @+id/textInputLayout3 : EditText
+        lateinit var @+id/textInputLayout4 : EditText
+        lateinit var @+id/textInputLayout5 : EditText
+        lateinit var @+id/constraintLayout3 : EditText
+        lateinit var @+id/textView12 : Button
+        lateinit var @+id/textView14 : Button
+
     }
 }
